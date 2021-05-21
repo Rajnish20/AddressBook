66fe6dc62cfdeff4a0d2cc19f11364f249672379
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-21 15:19:26 +0530

    Added contact class

---
+++ src/com/addressbook/entity/Contact.java
@@ -0,0 +1,20 @@
+package com.addressbook.entity;
+
+/**
+ * @author rajnish
+ *
+ */
+public class Contact {
+
+	/**
+	 * Details of a person that should be added in address book
+	 */
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int pincode;
+	private int mobileNo;
+	
+}
