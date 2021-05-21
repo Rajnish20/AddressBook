2760d2803ecac2f6d68f6654b12645fa7485ecbb
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-21 15:42:51 +0530

    Added options for users

--- src/com/addressbook/main/AddressBook.java
+++ src/com/addressbook/main/AddressBook.java
@@ -1,8 +1,20 @@
 package com.addressbook.main;
 
+import java.util.Scanner;
+
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+		
+		System.out.println("Choose Options : ");
+		System.out.println("Enter 1 to add contact in Address Book");
+		System.out.println("Enter 2 to delete contact from Address Book");
+		System.out.println("Enter 3 to edit a contact from Address Book");
+		
+		Scanner scanner = new Scanner(System.in);
+		int option = scanner.nextInt();
+		System.out.println(option);
+		
 	}
 
 }
